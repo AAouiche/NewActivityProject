edcebdf74a362a9dd348b7e1b1a2e2f5f1f177de
Akram Aouiche <akram-aouiche@hotmail.com>
2023-12-06 17:58:33 +0000

    cors change

--- NewActivityProject/Program.cs
+++ NewActivityProject/Program.cs
@@ -35,7 +35,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(
         builder =>
         {
-            builder.WithOrigins("http://localhost:5173")
+            builder.WithOrigins("https://reactactivities-production.up.railway.app/")
                    .AllowAnyHeader()
                    .AllowAnyMethod()
                    .AllowCredentials();
